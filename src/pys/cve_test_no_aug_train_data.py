ed139f19871e7501563be25a6490ebec9a9820e4
tamaricoh <106402042+tamaricoh@users.noreply.github.com>
2024-12-31 19:53:06 +0200

    GPU code

--- src/pys/cve_test_no_aug_train_data.py
+++ src/pys/cve_test_no_aug_train_data.py
@@ -1,40 +1,21 @@
 import pandas as pd
 
-df = pd.read_csv('../csv/dataset.csv')
-
-labels = df['Artifact Id']
-label_counts = labels.value_counts()
-
-filtered_labels_at_least_5 = label_counts[label_counts >= 5]
-filtered_labels_at_least_5_list = filtered_labels_at_least_5.index.tolist()
-
-filtered_df = df[df['Artifact Id'].isin(filtered_labels_at_least_5_list)]
-
-filtered_labels_balance = label_counts[(
-    label_counts >= 5) & (label_counts <= 200)]
-filtered_labels_balance_list = filtered_labels_balance.index.tolist()
-
-filtered_balance_df = df[df['Artifact Id'].isin(filtered_labels_balance_list)]
-
-command_df = df[df['Artifact Id'] == 'd3f:Command']
-sample_size = min(len(command_df), 16)
-sampled_command_df = command_df.sample(n=sample_size, random_state=42)
-
-train_df = pd.concat([filtered_balance_df, sampled_command_df])
+train_df = pd.read_csv('../csv/dataset.csv')
+train_labels = train_df['Artifact Id']
+label_counts = train_labels.value_counts()
+filtered_labels = label_counts[(label_counts >= 5) & (label_counts <= 200)]
+filtered_labels_list = filtered_labels.index.tolist()
+train_df = train_df[train_df['Artifact Id'].isin(filtered_labels_list)]
 train_df.reset_index(drop=True, inplace=True)
 
 test_df = pd.read_csv('../csv/cve_data.csv')
-
 test_labels = test_df['Artifact Id']
 test_labels_counts = test_labels.value_counts()
-test_labels_list = test_labels_counts.tolist()
-
-unique_train_labels = filtered_labels_balance.unique().tolist()
+unique_train_labels = train_labels.unique().tolist()
 unique_test_labels = test_labels.unique().tolist()
-
-all_unique_labels = list(set(unique_train_labels + unique_test_labels))
-
+all_unique_labels = list(
+    set([str(label) for label in unique_train_labels + unique_test_labels]))
 label_mapping = {label: idx for idx, label in enumerate(all_unique_labels)}
 
-train_df.reset_index(drop=True, inplace=True)
 test_df.reset_index(drop=True, inplace=True)
+print(label_mapping)
